6580bd81627682164fe70e844d2dafa86a071624
mkushwah355 <101509853+mkushwah355@users.noreply.github.com>
2022-03-30 19:46:00 +0530

    SelePra1stCommit

---
+++ src/test/java/DemoProject/SeleniumPractice/ChromeLaunch.java
@@ -0,0 +1,22 @@
+package DemoProject.SeleniumPractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeLaunch {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\mayan\\workspace\\libs\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+			driver.get("https://www.google.com");
+			String s1= driver.getTitle();
+			System.out.println("page title is: "  + s1);
+			driver.findElement(By.xpath("//*[@class='FPdoLc lJ9FBc']//center/input[1]")).submit();
+			driver.close();
+			
+		
+
+	}
+
+}
---
+++ src/test/java/DemoProject/SeleniumPractice/ErrorTableXpath.java
@@ -0,0 +1,22 @@
+package DemoProject.SeleniumPractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ErrorTableXpath {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\mayan\\workspace\\libs\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		String url = "https://semantic-ui.com/collections/table.html#error";
+		driver.get(url);
+		driver.manage().window().maximize();
+		String s1=driver.findElement(By.xpath("//*[@class='error']//parent::tbody/tr[3]/td[3]//preceding-sibling::td[2]")).getText();
+		System.out.println("row: " + s1);
+		driver.close();
+		
+
+	}
+
+}
