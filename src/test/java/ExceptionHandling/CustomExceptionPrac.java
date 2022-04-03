5923bca9b3eb0c1d8bf75cba5ee3fd1b52ad132c
mkushwah355 <101509853+mkushwah355@users.noreply.github.com>
2022-04-04 02:33:52 +0530

    c

---
+++ src/test/java/ExceptionHandling/CustomExceptionPrac.java
@@ -0,0 +1,36 @@
+package ExceptionHandling;
+
+public class CustomExceptionPrac  
+{  
+  
+    // method to check the age  
+    static void validate (int age) throws invalidAge{    
+       if(age < 18){  
+  
+        // throw an object of user defined exception  
+        throw new invalidAge("age is not valid to vote");    
+    }  
+       else {   
+        System.out.println("welcome to vote");   
+        }   
+     }    
+  
+    // main method  
+    public static void main(String args[])  
+    {  
+        try  
+        {  
+            // calling the method   
+            validate(13);  
+        }  
+        catch (invalidAge ex)  
+        {  
+            System.out.println("Caught the exception");  
+    
+            // printing the message from InvalidAgeException object  
+            System.out.println("Exception occured: " + ex);  
+        }  
+  
+        System.out.println("rest of the code...");    
+    }  
+}  
